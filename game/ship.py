2fa7b90679ff507b1dcb91299374c2dfcf554495
sujanlam <106173760+sujanlam@users.noreply.github.com>
2024-02-10 18:34:21 -0600

    Update ship.py

--- game/ship.py
+++ game/ship.py
@@ -2,7 +2,7 @@ import pygame
 
 
 class Ship():
-
+    # Building a ship with its details and location
     def __init__(self, ai_settings, screen):
         """Initialize the ship and set its starting position."""
         self.screen = screen
